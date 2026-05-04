f3ad73ee21d3b639aa191a1731cb25e8f72d6a6d
BelliaNino <bellianino1996@gmail.com>
2026-05-04 17:23:26 +0200

    add form layout with bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
 import AppForm from "./components/AppForm"
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+const libri = [
+  {
+    serialNumber: "AX-9821-B",
+    title: "Il Signore degli Anelli",
+    author: "J.R.R. Tolkien",
+    genre: "Fantasy"
+  },
+  {
+    serialNumber: "ZK-4430-C",
+    title: "Neuromante",
+    author: "William Gibson",
+    genre: "Cyberpunk"
+  },
+  {
+    serialNumber: "RM-1129-F",
+    title: "Assassinio sull'Orient Express",
+    author: "Agatha Christie",
+    genre: "Giallo"
+  }
+];
+
+
 function App() {
   
 
   return (
     <>
-     <AppForm/>
+     <AppForm libri={libri}/>
     </>
   )
 }
--- src/components/AppForm.jsx
+++ src/components/AppForm.jsx
@@ -1,5 +1,49 @@
 import { useState } from 'react';
 
-export default function AppForm() {
-  
+export default function AppForm({ libri }) { // Nota le graffe se passi libri come prop
+  return ( // <--- Fondamentale: aggiungi il return
+    <>
+      <div className='container'>
+        <form>
+          <div className="mb-3">
+            <label htmlFor="code" className="form-label">Serial Number</label>
+            <input 
+              type="text" 
+              className="form-control" 
+              id="serialNumber" 
+              placeholder='HB-3432-K5'
+            /> 
+          </div>
+          <div className="mb-3">
+            <label htmlFor="titlebook" className="form-label">Book Title</label>
+            <input 
+              type="text" 
+              className="form-control" 
+              id="titlebook" 
+              placeholder='I Pilastri della Terra'
+            /> 
+          </div>
+          <div className="mb-3">
+            <label htmlFor="genre" className="form-label">Author</label>
+            <input 
+              type="text" 
+              className="form-control" 
+              id="author" 
+              placeholder='Ken Follett'
+            /> 
+          </div>
+          <div className="mb-3">
+            <label htmlFor="genre" className="form-label">Book Genre</label>
+            <input 
+              type="text" 
+              className="form-control" 
+              id="bookGenre" 
+              placeholder='Romanzo Storico'
+            /> 
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    </>
+  );
 }
